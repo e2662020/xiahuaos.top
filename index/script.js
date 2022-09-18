1929669a96fff7be0ea5778116d57680c0c48317
e2662020 <e2662020@outlook.com>
2022-09-18 11:38:43 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index/script.js

--- index/script.js
+++ index/script.js
@@ -1,5 +1,5 @@
-var isMobile = device.mobile(),
-	isTable = device.tablet();
-if(isMobile){
-	window.open("http://m.xiahuaos.top","_self"); //如果终端是手机或者平板，就打开m.html
-}
+// var isMobile = device.mobile(),
+// 	isTable = device.tablet();
+// if(isMobile){
+// 	window.open("http://m.xiahuaos.top","_self"); //如果终端是手机或者平板，就打开m.html
+// }
